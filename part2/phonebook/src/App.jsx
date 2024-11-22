de96ab9b640e92efec1b7a7df26ca55dec03dcc1
Brandon Fernandez <54313265+BrandonFernandez1@users.noreply.github.com>
2024-11-22 14:48:01 +0800

    saving

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [lastID, setLastID] = useState(5)
   const [showNames, setShowNames] = useState(persons);
+
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(hook, [])
+  console.log(persons);
 
   const handleChange = (event) => {    
     const { name, value } = event.target;
@@ -26,7 +35,8 @@ const App = () => {
       return;
     }
 
-    setPersons(persons.concat({ name: newName, number: newNumber }));
+    setPersons(persons.concat({ name: newName, number: newNumber, id: lastID }));
+    setLastID(lastID + 1);
     setNewName('');
     setNewNumber('');
   }
